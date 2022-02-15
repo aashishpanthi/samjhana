80c6aeeccee0497398f61192bda07e647a773fb5
Aashish Panthi <aashishpanthi11@gmail.com>
2022-02-15 22:51:54 +0545

    add local fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Cookie/Cookie-Regular.ttf';
+import './fonts/Crete_Round/CreteRound-Italic.ttf';
+import './fonts/Roboto/Roboto-Medium.ttf';
 
 ReactDOM.render(
     <App />,
