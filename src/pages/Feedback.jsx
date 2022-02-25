5ab7fe52ab81fc4d921fa6c4bfff75349c60528f
Aashish Panthi <aashishpanthi11@gmail.com>
2022-02-25 22:07:27 +0545

    fix feedback form

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,48 +1,57 @@
 import { TextField, Button } from "@mui/material";
 import { useState } from "react";
 import "./styles/Feedback.css";
-import swal from 'sweetalert'
+import swal from "sweetalert";
 import { useNavigate } from "react-router-dom";
 
 const Feedback = () => {
   const [name, setName] = useState("Anonymous");
   const [message, setMessage] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) =>
-          encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "feedback": event.target.getAttribute("name"),
-        ...name,
-      }),
-    })
-      .then(() =>{
-        swal({
+
+    const formElements = [...event.currentTarget.elements];
+    const isValid =
+      formElements.filter((elem) => elem.name === "bot-field")[0].value === "";
+
+    const validFormElements = isValid ? formElements : [];
+
+    if (validFormElements.length < 1) {
+      swal("Invalid form", "Please fill out the form", "error");
+    } else {
+      const filledOutElements = validFormElements
+        .filter((elem) => !!elem.value)
+        .map(
+          (element) =>
+            encodeURIComponent(element.name) +
+            "=" +
+            encodeURIComponent(element.value)
+        )
+        .join("&");
+
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: filledOutElements,
+      })
+        .then(() => {
+          swal({
             text: "Thank you for your feedback!",
             buttons: {
-                cancel : {
-                    text: "Cancel",
-                    value: null,
-                    visible: true,
-                }
+              cancel: {
+                text: "Cancel",
+                value: null,
+                visible: true,
+              },
             },
             icon: "success",
+          });
+          navigate("/");
         })
-        navigate("/")
-      } )
-      .catch((error) => alert(error));
+        .catch((error) => swal(error));
+    }
   };
 
   return (
@@ -54,6 +63,8 @@ const Feedback = () => {
       onSubmit={handleSubmit}
     >
       <input type="hidden" name="form-name" value="feedback" />
+
+      <input name="bot-field" hidden />
 
       <p className="form-head">
         We will be more than happy to receive your feedback.
